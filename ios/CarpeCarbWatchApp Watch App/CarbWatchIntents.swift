9d9964dcbab60a02044b551a65f686162113a7b4
James Aguero <james.aguero@gmail.com>
2026-02-25 15:56:33 -0600

    rebase

---
+++ ios/CarpeCarbWatchApp Watch App/CarbWatchIntents.swift
@@ -0,0 +1,3 @@
+// Shared logic (CarbDataStore, EnvReader, PerplexityClient, LogFoodIntent,
+// CheckCarbsIntent) lives in CarbShared.swift which is compiled into both
+// the Runner and Watch targets.
